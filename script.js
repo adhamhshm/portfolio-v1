6d58460c7614fca73da3e9023879e319d59ab6e8
Adham <adham00109@gmai.com>
2023-08-13 09:04:11 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMenu() {
+
+    //target an element on webpage
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+
+    //when clicked, it will add or remove "open" class in the element
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+function toggleTheme() {
+    const theme = document.body;
+    theme.classList.toggle("dark-mode");
+ }
